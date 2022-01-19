7cdc8777b34fa4fc7d2b958e11477d888c81249b
milesmaroney <miles@milesmaroney.com>
2022-01-19 15:12:41 -0800

    Login updates

--- src/Login.js
+++ src/Login.js
@@ -10,7 +10,15 @@ function Login(props) {
   const [newPassword, setNewPassword] = React.useState('');
   const [confirmNewPassword, setConfirmNewPassword] = React.useState('');
 
+  const [showError, setShowError] = React.useState(false);
+  const [errorMessage, setErrorMessage] = React.useState('');
+
   function handleLogin() {
+    if (!username || !password) {
+      setErrorMessage('Complete All Fields');
+      handlePopup();
+      return;
+    }
     axios
       .get(`${config.server}/api/login/${username}/${password}`)
       .then((res) => {
@@ -18,10 +26,26 @@ function Login(props) {
           props.setUser(res.data);
         }
       })
-      .catch((err) => console.error('Incorrect Login', err));
+      .catch((err) => {
+        console.error('Bad Login');
+        setErrorMessage('Incorrect Login');
+        handlePopup();
+      });
+  }
+
+  function handlePopup() {
+    setShowError(true);
+    setTimeout(() => {
+      setShowError(false);
+    }, 5000);
   }
 
   function handleSignup() {
+    if (!newUsername || !newPassword || !confirmNewPassword) {
+      setErrorMessage('Complete All Fields');
+      handlePopup();
+      return;
+    }
     if (newPassword === confirmNewPassword) {
       axios
         .post(`${config.server}/api/signup`, {
@@ -36,49 +60,82 @@ function Login(props) {
             workouts: res.data.workouts,
           });
         })
-        .catch((err) => console.error(err));
+        .catch((err) => {
+          console.error('User Exists');
+          setErrorMessage('User Exists');
+          handlePopup();
+        });
+    } else {
+      setErrorMessage('Passwords Dont Match');
+      handlePopup();
     }
   }
 
   return (
-    <div className='h-screen w-screen bg-slate-500'>
-      <img src={Logo} alt='workoutKeeper' className='mx-auto py-12 w-1/2' />
-      <div className='h-full w-full flex justify-evenly'>
-        <div className='w-1/3 flex flex-col'>
-          Username:
+    <div className='h-screen w-screen bg-neutral-800 text-white'>
+      <div className='mx-auto py-12 w-2/3 relative'>
+        <img src={Logo} alt='workoutKeeper' />
+        <div
+          className='absolute bottom-0 left-1/2 -translate-x-1/2 text-sm md:text-base border border-red-600 text-red-600 font-bold px-4 py-2 min-w-fit rounded transition-all duration-300'
+          style={{
+            opacity: showError ? '1' : '0',
+            display: showError ? '' : 'hidden',
+          }}
+        >
+          {errorMessage}
+        </div>
+      </div>
+      <div className='md:h-full w-full flex flex-col md:flex-row md:justify-evenly'>
+        <div className='px-4 md:px-0 md:w-1/3 flex flex-col'>
+          <div className='font-bold pt-4'>Username:</div>
           <input
-            type='text'
+            className='input rounded'
             value={username}
             onChange={(e) => setUsername(e.target.value)}
           />
-          Password:
+          <div className='font-bold pt-4'>Password:</div>
           <input
+            className='input rounded'
             type='password'
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
-          <button onClick={handleLogin}>Login</button>
+          <button
+            className='rounded w-full font-bold mt-2'
+            style={{ backgroundColor: 'rgb(240, 20, 60)' }}
+            onClick={handleLogin}
+          >
+            Login
+          </button>
         </div>
-        <div className='w-1/3 flex flex-col'>
-          Username:
+        <div className='pt-12 md:pt-0 px-4 md:px-0 md:w-1/3 flex flex-col'>
+          <div className='font-bold pt-4'>Username:</div>
           <input
-            type='text'
+            className='input rounded'
             value={newUsername}
             onChange={(e) => setNewUsername(e.target.value)}
           />
-          Password:
+          <div className='font-bold pt-4'>Password:</div>
           <input
+            className='input rounded'
             type='password'
             value={newPassword}
             onChange={(e) => setNewPassword(e.target.value)}
           />
-          Password:
+          <div className='font-bold pt-4'>Confirm Password:</div>
           <input
+            className='input rounded'
             type='password'
             value={confirmNewPassword}
             onChange={(e) => setConfirmNewPassword(e.target.value)}
           />
-          <button onClick={handleSignup}>Sign Up</button>
+          <button
+            className='rounded w-full font-bold mt-2'
+            style={{ backgroundColor: 'rgb(240, 20, 60)' }}
+            onClick={handleSignup}
+          >
+            Sign Up
+          </button>
         </div>
       </div>
     </div>
